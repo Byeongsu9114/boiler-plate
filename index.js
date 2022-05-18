175a8c2997256665950613cd3485b63bd1cdb65e
Byeongsu9114 <byeongsu9114@nate.com>
2022-05-18 13:10:00 +0900

    secret HEROKU

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://byeongsu9114:1234@boiler-plate.906rc.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! 안녕하세요');
 });
+
+app.post('/register', (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
